13403352450b56db8f4821a24d068f26d4cf82ce
frida25130500 <alu.25130500@correo.itlalaguna.edu.mx>
2026-09-11 12:52:29 -0600

    Guardar cambios

--- MyApp_01/Form1.Designer.cs
+++ MyApp_01/Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnProceso = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nombre";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(75, 71);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(179, 22);
+            this.txtNombre.TabIndex = 4;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // btnProceso
+            // 
+            this.btnProceso.Location = new System.Drawing.Point(75, 123);
+            this.btnProceso.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnProceso.Name = "btnProceso";
+            this.btnProceso.Size = new System.Drawing.Size(179, 44);
+            this.btnProceso.TabIndex = 3;
+            this.btnProceso.Text = "Enviar";
+            this.btnProceso.UseVisualStyleBackColor = true;
+            this.btnProceso.Click += new System.EventHandler(this.btnProceso_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnProceso);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnProceso;
     }
 }
 
--- MyApp_01/Form1.cs
+++ MyApp_01/Form1.cs
@@ -16,5 +16,21 @@ namespace MyApp_01
         {
             InitializeComponent();
         }
+
+        private void btnProceso_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            MessageBox.Show(nombre);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
