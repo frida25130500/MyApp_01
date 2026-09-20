b820026aece0ad785a4a26bc5e7051070c00518d
frida25130500 <alu.25130500@correo.itlalaguna.edu.mx>
2026-09-20 14:18:44 -0600

    Guardar cambios

--- MyApp_01/Form1.Designer.cs
+++ MyApp_01/Form1.Designer.cs
@@ -166,7 +166,7 @@
             this.ClientSize = new System.Drawing.Size(889, 504);
             this.Controls.Add(this.splitContainer1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Mi primer App (DataGrid)";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel1.PerformLayout();
